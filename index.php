47ee9c1fd8375410fae710e78e6d2476aa9af318
onizuka17 <onizuka17@users.noreply.github.com>
2020-04-14 19:12:48 +0700

    First push

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Tính kế thừa
+ */
+class DongVat 
+{
+    /**
+     * summary
+     */
+    
+    public $ten;
+    protected $tuoi;
+    private $loai; 
+
+    public function __construct($ten,$tuoi,$loai)
+    {
+		$this->ten = $ten;
+		$this->tuoi = $tuoi;
+		$this->loai = $loai;        
+    }
+
+    public function getTen(){
+    	return $this->ten;
+    }
+
+    protected function getTuoi(){
+    	return $this->tuoi;
+    }
+
+    private function getloai()
+    {
+    	return $this->ten;
+    }
+
+}
+
+
+$dongvat = new DongVat('dog','10','covu');
+echo $dongvat->getTen();
